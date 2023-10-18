b5e1f4fe60a22fcd92aec244885b3e4e7ab5345a
acodercat <codercat@163.com>
2023-10-18 14:45:56 +0100

    version 2.

---
+++ web/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Counter: {{ count }}</h1>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<style scoped>
+button {
+  margin-top: 10px;
+  padding: 5px 15px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
